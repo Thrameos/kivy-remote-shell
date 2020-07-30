71ac73a428f19aaa3e9eb552cbf0540c6d348ddf
Karl E. Nelson <nelson85@llnl.gov>
2020-07-29 18:12:15 -0700

    Apply the patch fixes

--- local/jpype/__init__.py
+++ local/jpype/__init__.py
@@ -2,6 +2,7 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 from pythonforandroid.toolchain import shprint, current_directory, info
 from pythonforandroid.patching import will_build
 import sh
+import os
 from os.path import join
 import glob
 
@@ -12,7 +13,7 @@ class JPypeRecipe(CppCompiledComponentsPythonRecipe):
     name = 'jpype'
     depends = []
     site_packages_name = 'jpype'
-    patches = []
+    patches = ['patch']
 
     call_hostpython_via_targetpython = False
     ignore_setup_py = False
@@ -58,6 +59,22 @@ class JPypeRecipe(CppCompiledComponentsPythonRecipe):
         info("From %s"%self.get_build_dir(arch.arch))
         with current_directory(self.get_build_dir(arch.arch)):
             shprint(sh.cp, '-r', join('build','temp.linux-x86_64-3.8','org.jpype','classes','org'), self.ctx.javaclass_dir)
+
+    def apply_patches(self, arch, build_dir=None):
+        super().apply_patches(arch, build_dir)
+        build_dir = build_dir if build_dir else self.get_build_dir(arch.arch)
+
+        # Remove modules that do not apply
+        shprint(sh.rm, "-f", join(build_dir, "jpype", "pickle.py"))
+        shprint(sh.rm, "-f", join(build_dir, "jpype", "beans.py"))
+        shprint(sh.rm, "-f", join(build_dir, "jpype", "imports.py"))
+        shprint(sh.rm, "-f", join(build_dir, "jpype", "_classpath.py"))
+        shprint(sh.rm, "-f", join(build_dir, "jpype", "_gui.py"))
+        shprint(sh.rm, "-f", join(build_dir, "native", "java", "org", "jpype","JPypeSignal.java"))
+        shprint(sh.rm, '-Rf', join(build_dir, "native", "java", "org", "jpype","html"))
+        shprint(sh.rm, '-Rf', join(build_dir, "native", "java", "org", "jpype","javadoc"))
+        shprint(sh.rm, '-Rf', join(build_dir, "native", "java", "org", "jpype","classloader"))
+
 
 
     # def postbuild_arch(self, arch):
